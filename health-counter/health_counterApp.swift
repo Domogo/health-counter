36246c3288aa5bded4ed41fdc067bde9248433bd
Domogo <domsatrapa@gmail.com>
2024-10-01 16:47:32 +0200

    Initial Commit

---
+++ health-counter/health_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_counterApp.swift
+//  health-counter
+//
+//  Created by Domagoj Satrapa on 01.10.2024..
+//
+
+import SwiftUI
+
+@main
+struct health_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ health-counterTests/health_counterTests.swift
@@ -0,0 +1,17 @@
+//
+//  health_counterTests.swift
+//  health-counterTests
+//
+//  Created by Domagoj Satrapa on 01.10.2024..
+//
+
+import Testing
+@testable import health_counter
+
+struct health_counterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
